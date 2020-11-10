b4bb32a221447e815fd720d15964d948a240ae8f
Nguyen Khac Khanh (Encacap) <encacap@gmail.com>
2020-11-11 01:02:39 +0700

    Show month with the highest value in 2020

--- src/exercises/month_value.py
+++ src/exercises/month_value.py
@@ -29,6 +29,27 @@ def main():
     for month in valuePerMonths:
         print(month)
 
+    print("--------------------------")
+    print("Thang co gia tri don dat hang lon nhat cua nam 2020: ")
+
+    highestMonthValue = database.selectOne('''
+        SELECT MAX(TTG)
+        FROM (
+            SELECT D.MONTH, SUM(D.GTDDH) AS TTG
+            FROM (
+                SELECT strftime("%m", A.NGAYDH) AS MONTH, SUM(B.SLDAT * C.DONGIA) AS GTDDH
+                FROM DDH A, CTDDH B, HANG C
+                WHERE strftime("%Y", A.NGAYDH) = "2020"
+                    AND A.MADDH = B.MADDH
+                    AND B.MAHG = C.MAHG
+                GROUP BY A.MADDH
+            ) D
+            GROUP BY D.MONTH
+        )
+    ''')
+
+    print(highestMonthValue)
+
     database.terminate()
 
 
